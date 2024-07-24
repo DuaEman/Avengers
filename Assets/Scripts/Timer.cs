11164506ad01e59690f3e2fa19ede630107021b3
Dua e Eman <emandua698@gmail.com>
2024-07-24 22:31:32 +0500

    new1

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -7,19 +6,38 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerTime;
     [SerializeField] float remainingTime;
+    [SerializeField] GameObject gameOverPanel;
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
     void Update()
     {
-        if(remainingTime > 0)
+        if (isGameOver)
+            return;
+
+        if (remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
         }
-        else if ( remainingTime < 0)
+        else
         {
             remainingTime = 0;
-            
+            GameOver();
         }
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerTime.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+        timerTime.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0; // Stop the game
     }
 }
