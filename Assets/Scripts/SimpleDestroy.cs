5bb7e8351810181c81a782cce031424fd9acf681
Dua e Eman <emandua698@gmail.com>
2024-08-05 21:07:26 +0500

    particle effects

--- Assets/Scripts/SimpleDestroy.cs
+++ Assets/Scripts/SimpleDestroy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SimpleDestroy : MonoBehaviour
 {
     private ONDestroy objectManager;
+    public GameObject particleEffect; // Reference to the particle effect prefab
 
     private void Start()
     {
@@ -14,6 +15,28 @@ public class SimpleDestroy : MonoBehaviour
         if (objectManager != null)
         {
             objectManager.HandleCollision(gameObject, collision.gameObject.tag);
+
+            // Instantiate the particle effect at the object's position and rotation
+            if (particleEffect != null)
+            {
+                GameObject instantiatedParticleEffect = Instantiate(particleEffect, transform.position, transform.rotation);
+
+                // Get the ParticleSystem component to determine its duration
+                ParticleSystem ps = instantiatedParticleEffect.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    // Destroy the particle effect after it finishes playing
+                    Destroy(instantiatedParticleEffect, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+                else
+                {
+                    // If no ParticleSystem component is found, destroy it after a default duration
+                    Destroy(instantiatedParticleEffect, 2f); // Adjust the default duration if necessary
+                }
+            }
+
+            // Destroy the game object
+            Destroy(gameObject);
         }
     }
 }
